907da9a38194f5cb6ab3def154403ce703434625
ShenbagavalliGovindharasu <88303324+ShenbagavalliGovindharasu@users.noreply.github.com>
2022-01-06 10:43:06 +0530

    first commit

---
+++ UniversityCourse/src/main/java/com/univ/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.univ.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.univ.entity.Course;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+}
